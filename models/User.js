b0c0f8ac44ebb6e228f16752efd0e7d85a8a1534
danid757 <dolandanika@gmail.com>
2022-09-22 13:41:28 -0500

    create user schema

---
+++ models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const Scehma = mongooose.Scehma;
+
+//create schema
+const UserScehma = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  avatar: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = User = mongoose.model("users", UserScehma);
