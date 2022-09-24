f040cdf9a96894595c418710b8bcb42d07dd7e0d
danid757 <dolandanika@gmail.com>
2022-09-24 17:07:12 -0500

    add JWT

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://danidolan757:Iloveyousomuch757@devconnector.jclkhjx.mongodb.net/?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://danidolan757:Iloveyousomuch757@devconnector.jclkhjx.mongodb.net/?retryWrites=true&w=majority',
+    secretOrKey: 'secret'
 };
